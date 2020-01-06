7b06dbe2b60b17ff16127edc05c4f5a624201265
LuGuoqiang <luguoqiang_vendor@sensetime.com>
2020-01-06 14:02:37 +0800

    新增的动态域名

--- app/src/main/java/com/lugq/appframedemo/constant/Constants.java
+++ app/src/main/java/com/lugq/appframedemo/constant/Constants.java
@@ -2,11 +2,13 @@ package com.lugq.appframedemo.constant;
 
 import android.os.Environment;
 
+import com.lugq.appframedemo.BuildConfig;
+
 import java.io.File;
 
 public class Constants {
 
-    public static String BASE_URL = "https://api.github.com";
+    public static String BASE_URL = BuildConfig.DOMAIN;
 
     //路径分隔符
     private static final String SEPARATOR = File.separator;
