f1656b3e66ded5f5242b3b3f337a6c870a7e9a8d
luguoqiang <>
2020-09-11 14:09:30 +0800

    代码优化

--- app/src/main/java/com/lugq/appframedemo/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/lugq/appframedemo/ui/fragment/HomeFragment.kt
@@ -7,13 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import com.lugq.appframedemo.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -27,7 +24,6 @@ class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
